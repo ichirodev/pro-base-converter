5a0d633814ce9e330ae6642e96d4f52df23a7030
ichirodev <adanschiffer@hotmail.com>
2020-10-25 22:17:47 -0600

    feat(convertion function)
    Added convertBase function which converts from base x to y
    * You can find said function in the calculation.js file

--- calculation.js
+++ calculation.js
@@ -1,3 +1,25 @@
-const dictionary = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~";
-const maxBase = dictionary.length;
+function convertBase(value, iBase, oBase) {
+    const dictionary = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~";
+    const maxBase = dictionary.length;
+    
+    if (oBase >= maxBase || iBase >= maxBase) {
+        throw new Error("Invalid base, max base " + maxBase + "\n");
+        return '0';
+    }
+    var r = dictionary.split('');
+    var iRange = r.slice(0, iBase);
+    var oRange = r.slice(0, oBase);
 
+    var foo = value.split('').reverse().reduce((carry, digit, index) => {
+        if (iRange.indexOf(digit) === -1) throw new Error("Invalid digit " + digit + "\n");
+        return carry += iRange.indexOf(digit) * Math.pow(iBase, index);
+    }, 0);
+
+    var newValue = '';
+    while(foo > 0) {
+        newValue = oRange[foo % oBase] + newValue;
+        foo = (foo - (foo % oBase)) / oBase;
+    }
+    
+    return newValue || '0';
+}
